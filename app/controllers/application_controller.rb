76cb4aa9c1e3d57dae204c7e457398d87a93e6ff
Gonzalo Franco <gfrancomontero@gmail.com>
2020-09-01 15:28:15 +0200

    next step dashboard routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # def after_sign_in_path_for(resource)
+  #   if resource.admin
+  #     redirect_to
+  #   else
+  #     redirect_to
+  #   end
+  # end
 end
