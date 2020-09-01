672f0fa1907df206ece46ddacce15f897ac0d1ad
Martin Aviles <mgmbamartin@gmail.com>
2020-09-01 12:47:08 +0200

    create method

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,10 +1,14 @@
 class OrderItemsController < ApplicationController
   def create
-    order_item = OrderItem.new(strong_params)
-    order_item.order = current_user.orders.last
-  end
-
-  def strong_params
-    #TODO
+    order_item = OrderItem.new(parms[:quantity])
+    order_item.menu_item = MenuItem.find(params[:id])
+    if current_user.orders.last.status == "in progress"
+      order_item.order = current_user.orders.last
+    else
+      order = Order.new()
+      order.save!
+      order_item.order = current_user.orders.last
+    end
+    order_item.save!
   end
 end
