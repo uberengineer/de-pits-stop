a526de94fb9bc70613832a7e492b415e2c3670ea
Martin Aviles <mgmbamartin@gmail.com>
2020-09-02 11:58:14 +0200

    started validation

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,14 @@
 class OrderItem < ApplicationRecord
+  before_validation :check_if_exists_in_order
   belongs_to :order
   belongs_to :menu_item
+
+  def check_if_exists_in_order
+    if OrderItems.find_by(order_id: self.order_id, menu_item_id: self.menu_item.id)
+      self.quantity =  self.quantity + OrderItems.find_by(order_id: self.order_id, menu_item_id: self.menu_item.id).quantity
+      OrderItems.find_by(order_id: self.order_id, menu_item_id: self.menu_item.id).destroy
+    end
+  end
 end
+
+
