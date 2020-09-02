6b15ff899c20e03ec8f4b1edf81a30f60bdd2a3c
Martin Aviles <mgmbamartin@gmail.com>
2020-09-02 12:35:16 +0200

    finish functionality for past orders and today's orders

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/dashboard", to: "pages#dashboard", as: "dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :menu_items , only: [:index, :show, :new, :create]
+  get "/past_orders", to: "orders#past_orders", as: "past_orders"
   resources :orders, only: [:show, :update, :index] do
     member do
       resources :order_items, only: [:create, :destroy]
