7b427c54258f6c65ccb6c07a57235108c4af9c17
Gonzalo Franco <gfrancomontero@gmail.com>
2020-09-03 17:51:06 +0200

    finalizing

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,12 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+// const view_cart = document.getElementById('cart-link')
+// const add_to_cart = document.getElementById('add-to-cart')
+
+// add_to_cart.addEventListener('click', function() {
+//   console.log("licked")
+//   view_cart.classList.remove("d-none")
+// });
+
