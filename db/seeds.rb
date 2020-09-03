34c21d8c3984a513e9af2182da88e085854d363c
Chris Kirk <uberengineer@gmail.com>
2020-09-03 15:24:32 +0200

    recommiting seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
 require "open-uri"
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 menu = YAML.load_file(Rails.root.join("db/assets/menu_items.yml")).deep_symbolize_keys
 MenuItem.destroy_all
 Cloudinary::Api.delete_all_resources
