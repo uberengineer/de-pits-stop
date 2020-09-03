07b03534bbe1a79354072ac9aa89d71f822d08e3
Chris Kirk <uberengineer@gmail.com>
2020-09-03 17:14:01 +0200

    added edit menu functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   authenticate :user, ->(user) { user.admin? } do
     mount Blazer::Engine, at: "blazer"
