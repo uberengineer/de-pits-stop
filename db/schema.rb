d190592ec7472de3d98a6608739633dca1eb2a24
Chris Kirk <uberengineer@gmail.com>
2020-09-04 14:15:48 +0200

    group merge

--- db/schema.rb
+++ db/schema.rb
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(version: 2020_09_04_093228) do
 
   create_table "orders", force: :cascade do |t|
     t.string "comment"
-    t.string "status"
+    t.string "status", default: "in progress"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
