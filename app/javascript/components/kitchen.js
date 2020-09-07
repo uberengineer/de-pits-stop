e8f70e0964a3e561cee5c51e799ddc2ddbddc79d
Gonzalo Franco <gfrancomontero@gmail.com>
2020-09-07 11:35:41 +0200

    next step activate kitchen open toggle button

---
+++ app/javascript/components/kitchen.js
@@ -0,0 +1,9 @@
+// let checkbox = document.getElementById("kitchen").addEventListener('change', function() {
+//      if(this.checked) {
+//        alert("Checked!");
+//         // Checkbox is checked..
+//     } else {
+//         alert("UnChecked!");
+//         // Checkbox is not checked..
+//     }
+// });
