9c2f9ced2739bf6c7744ff3b65fd0e98570c4e48
Gonzalo Franco <gfrancomontero@gmail.com>
2020-09-08 12:22:33 +0200

    mobile-responsive

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   # before_action :redirect_root_domain
   before_action :authenticate_user!
   before_action :set_kitchen
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :organization])
+  end
 
   def after_sign_in_path_for(resource)
     if resource.admin
