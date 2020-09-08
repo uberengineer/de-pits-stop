bd81a4636c68930f210b46406dd3cd3ff895de17
Chris Kirk <uberengineer@gmail.com>
2020-09-08 10:06:46 +0200

    change css loading from js to css pipeline

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,7 +25,7 @@ require("channels")
 
 import "bootstrap";
 import Swal from 'sweetalert2/dist/sweetalert2.js';
-import 'sweetalert2/src/sweetalert2.scss';
+// import 'sweetalert2/src/sweetalert2.scss';
 window.Swal = Swal;
 
 // Internal imports, e.g:
