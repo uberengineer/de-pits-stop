258b4263289fb8999bf833b1f2a17276be0719ed
Chris Kirk <uberengineer@gmail.com>
2020-09-08 17:32:45 +0200

    test payment set up

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :password => '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',
   :domain => 'localhost:3000',
   :address => 'smtp.sendgrid.net',
-  :port => 25, 587
+  :port => 25,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
