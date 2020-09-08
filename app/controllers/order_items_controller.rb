f231af2f5fbde92e233fbc6dbe4b1f2d20080f41
Chris Kirk <uberengineer@gmail.com>
2020-09-08 17:11:14 +0200

    stripe stuff added

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,21 +1,22 @@
 class OrderItemsController < ApplicationController
   def create
-    order_item = OrderItem.new()
+    order_item = OrderItem.new
     order_item.quantity = params[:order_item][:quantity]
     order_item.menu_item = MenuItem.find(params[:item_id])
-    order_item.order = Order.where(user: current_user, status: "in progress").first_or_create
-    if order_item.save
+    order_item.order = Order.find_or_create_by(user: current_user, status: "in progress")
+    order_item.save
+
+
       @order_item = order_item
-      # flash[:notice] = "Succesfully added to cart"
-      # GONZALO NOTE: Create a small javascript popup here.
-    else
-      # redirect menu_items_path
+
+
+
     end
   end
 
   def destroy
     order_item = OrderItem.find(params[:id])
     order_item.destroy!
-    redirect_to checkout_order_path(Order.where(user: current_user, status: "in progress").first_or_create)
+    redirect_to checkout_order_path(Order.find_or_create_by(user: current_user, status: "in progress"))
   end
 end
---
+++ config/initializers/mollie.rb
@@ -0,0 +1,6 @@
+# Mollie::Client.configure do |config|
+#   config.api_key = '<your-api-key>'
+#   # Timeouts (default - 60)
+#   # config.open_timeout = 60
+#   # config.read_timeout = 60
+# end
