d583c1dfe773fd4e7d46b91f78b131fc3e863882
Chris Kirk <uberengineer@gmail.com>
2020-10-29 16:11:52 +0100

    debugging

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -43,6 +43,7 @@ class OrdersController < ApplicationController
     payment = Mollie::Payment.create(
       amount:       { value: humanized_money(@order.amount), currency: 'EUR' },
       description:  @order.id.to_s,
+      raise
       redirect_url: confirmation_url(@order, host: root_url),
       webhook_url:  webhook_url(host: root_url)
       )
