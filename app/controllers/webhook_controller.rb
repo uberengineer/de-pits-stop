c761e995cc87613a1e24cd3e47df2ceceeb87f5c
Chris Kirk <uberengineer@gmail.com>
2020-10-29 15:39:24 +0100

    Added webhook controller

---
+++ app/controllers/webhook_controller.rb
@@ -0,0 +1,18 @@
+  class WebhookController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
+
+  def mollie
+    @payment = Mollie::Payment.get(params[:id])
+    @order = Order.find_by(mollie_id: @payment.id)
+    @order.payment_status = @payment.status
+    @order.save
+
+    if @order.payment_status == "paid"
+    OrderChannel.broadcast_to(
+       "orders",
+      render_to_string(partial: "orders/order", locals: { order: @order })
+      )
+    end
+  end
+end
