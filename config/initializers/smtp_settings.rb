8d4a62691a57ef652be6b264ad4cceb93942d28c
Chris Kirk <uberengineer@gmail.com>
2020-10-29 14:40:39 +0100

    updated dependancies

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   address: 'srv12243.hostingserver.nl',
   port: 587,
-  domain: 'depitskeuken.nl',
+  domain: ENV['DOMAIN'],
   user_name: ENV['MAILER_ADDRESS'],
   password: ENV['MAILER_PASSWORD'],
   authentication: :login,
