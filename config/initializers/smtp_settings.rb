c16443cf2a5f6e5404c004882c20ade79fa7451e
Chris Kirk <uberengineer@gmail.com>
2020-11-04 17:19:20 +0100

    removed domain

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,7 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   address: 'depitskeuken.nl',
   port: 25,
-  domain: 'srv12243.hostingserver.nl',
   user_name: ENV['MAILER_ADDRESS'],
   password: ENV['MAILER_PASSWORD'],
   authentication: :login,
