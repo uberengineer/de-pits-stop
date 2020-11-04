f2ead07f8a3b38e821a36533a3b153cde45159d7
Chris Kirk <uberengineer@gmail.com>
2020-11-04 17:31:07 +0100

    further smtp changes

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,6 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   address: 'depitskeuken.nl',
   port: 25,
+  domain: 'depitskeuken.nl',
   user_name: ENV['MAILER_ADDRESS'],
   password: ENV['MAILER_PASSWORD'],
   authentication: :login,
