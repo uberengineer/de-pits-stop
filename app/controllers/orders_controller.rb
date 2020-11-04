2074a55196a2d48cf1475c539231fde38f888dc3
Chris Kirk <uberengineer@gmail.com>
2020-11-04 19:36:53 +0100

    trying UserMailer arguments

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -48,11 +48,12 @@ class OrdersController < ApplicationController
       
       @order.mollie_id = payment.id
       @order.save
-      UserMailer.confirmation_email(@order.user, @order).deliver_later
+      # UserMailer.confirmation_email(@order.user, @order).deliver_now
       redirect_to payment.checkout_url
     elsif @order.status == "not ready"
       @order.status = "awaiting pick-up"
-      UserMailer.pick_up_email(@order.user).deliver_later
+      UserMailer.with(user: @order.user).pick_up_email.deliver_now
+      # UserMailer.pick_up_email(@order.user).deliver_now
       redirect_to orders_path
     else @order.status == "awaiting pick-up"
       @order.status = "completed"
