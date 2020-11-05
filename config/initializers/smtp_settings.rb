f09bbafcd15942a45205d81cdbf9df017e4acf8d
Chris Kirk <uberengineer@gmail.com>
2020-11-05 14:41:15 +0100

    port 587

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,9 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  address: 81.4.96.183,
-  port: 995,
+  address: '81.4.96.183',
+  port: 587,
   enable_starttls_auto: true,
   domain: 'srv12243.hostingserver.nl',
   user_name: ENV['MAILER_ADDRESS'],
   password: ENV['MAILER_PASSWORD'],
-  authentication: :plain
+  authentication: 'plain',
+  openssl_verify_mode: 'none'
 }
