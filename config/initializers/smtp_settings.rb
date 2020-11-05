ca4d34b3f2b12cc061167d2c8984548d384bd829
Chris Kirk <uberengineer@gmail.com>
2020-11-05 13:56:24 +0100

    added SMTP strings 2

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   address: 'mail.depitskeuken.nl',
   port: 587,
   enable_starttls_auto: true,
-  domain: depitskeuken.nl,
+  domain: 'depitskeuken.nl',
   user_name: ENV['MAILER_ADDRESS'],
   password: ENV['MAILER_PASSWORD'],
   authentication: login,
