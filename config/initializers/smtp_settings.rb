270f81c7d0888091bb7b217689f5b07f24c15255
Chris Kirk <uberengineer@gmail.com>
2020-11-05 14:22:23 +0100

    cram_md5 auth

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'mail.depitskeuken.nl',
   port: 587,
   enable_starttls_auto: true,
-  domain: 'depitskeuken.nl',
+  domain: 'srv12243.hostingserver.nl',
   user_name: ENV['MAILER_ADDRESS'],
   password: ENV['MAILER_PASSWORD'],
-  authentication: login,
+  authentication: :cram_md5,
 }
