f1430786f9fff228370c8469d6496b2927693603
Chris Kirk <uberengineer@gmail.com>
2020-11-05 15:35:24 +0100

    DO NOT PUSH MASTER

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'srv12243.hostingserver.nl',
   port: 587,
   enable_starttls_auto: true,
-  user_name: ENV['MAILER_ADDRESS'],
-  password: ENV['MAILER_PASSWORD'],
+  user_name: 'contact@depitskeuken.nl',
+  password: 'L&4k9kn4',
   authentication: 'plain',
   openssl_verify_mode: 'none'
 }
