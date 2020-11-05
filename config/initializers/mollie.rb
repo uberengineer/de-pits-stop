18cc23e34bed4131c17736a3801bf13db20d30f9
Chris Kirk <uberengineer@gmail.com>
2020-11-05 17:33:06 +0100

    finished ENV stuff

--- config/initializers/mollie.rb
+++ config/initializers/mollie.rb
@@ -1,5 +1,5 @@
 Mollie::Client.configure do |config|
-  config.api_key = ENV['MOLLIE_TEST_KEY']
+  config.api_key = ENV['MOLLIE_KEY']
   # Timeouts (default - 60)
   # config.open_timeout = 60
   # config.read_timeout = 60
