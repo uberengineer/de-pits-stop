c34137e2f15c5dff8103f7716075c8ac3294ce78
Chris Kirk <uberengineer@gmail.com>
2020-11-05 14:32:05 +0100

    POP3 and plain auth

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: 'mail.depitskeuken.nl',
-  port: 587,
+  address: 81.4.96.183,
+  port: 995,
   enable_starttls_auto: true,
   domain: 'srv12243.hostingserver.nl',
   user_name: ENV['MAILER_ADDRESS'],
   password: ENV['MAILER_PASSWORD'],
-  authentication: :cram_md5,
+  authentication: :plain
 }
