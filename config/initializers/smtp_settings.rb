39999e63a73b280a91e0133431a79f66ddfa51fb
Chris Kirk <uberengineer@gmail.com>
2020-11-05 15:12:56 +0100

    hostingserver address

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,8 +1,7 @@
 ActionMailer::Base.smtp_settings = {
-  address: '81.4.96.183',
+  address: 'srv12243.hostingserver.nl',
   port: 587,
   enable_starttls_auto: true,
-  domain: 'srv12243.hostingserver.nl',
   user_name: ENV['MAILER_ADDRESS'],
   password: ENV['MAILER_PASSWORD'],
   authentication: 'plain',
