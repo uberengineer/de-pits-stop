ea9f95a8cdec5e1602b9fa51284f900bb8d294ca
Chris Kirk <uberengineer@gmail.com>
2021-04-21 14:57:20 +0200

    missing extras in seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,6 @@ User.destroy_all
 Cloudinary::Api.delete_all_resources
 puts "starting the seeds"
 
-
 Kitchen.create()
 
 User.create(
@@ -25,7 +24,7 @@ User.create(
 puts "Creating menu items"
 menu = YAML.load_file(Rails.root.join("db/assets/menu_items.yml")).deep_symbolize_keys
 
-menu[:daily_specials].each do |menu_item|
+menu[:warme_gerechten].each do |menu_item|
   file = menu_item.delete(:image)
   f = MenuItem.create(menu_item)
   f.price_cents = menu_item[:price] * 100
@@ -33,7 +32,7 @@ menu[:daily_specials].each do |menu_item|
   puts "Created menu item #{menu_item[:name]}"
 end
 
-menu[:combi_deals].each do |menu_item|
+menu[:koude_gerechten].each do |menu_item|
   file = menu_item.delete(:image)
   f = MenuItem.create(menu_item)
   f.price_cents = menu_item[:price] * 100
@@ -41,7 +40,31 @@ menu[:combi_deals].each do |menu_item|
   puts "Created menu item #{menu_item[:name]}"
 end
 
-menu[:losse_items].each do |menu_item|
+menu[:soepen].each do |menu_item|
+  file = menu_item.delete(:image)
+  f = MenuItem.create(menu_item)
+  f.price_cents = menu_item[:price] * 100
+  f.image.attach(io: File.open(file), filename: "#{f.id}.jpeg", content_type: 'image/jpeg')
+  puts "Created menu item #{menu_item[:name]}"
+end
+
+menu[:salades].each do |menu_item|
+  file = menu_item.delete(:image)
+  f = MenuItem.create(menu_item)
+  f.price_cents = menu_item[:price] * 100
+  f.image.attach(io: File.open(file), filename: "#{f.id}.jpeg", content_type: 'image/jpeg')
+  puts "Created menu item #{menu_item[:name]}"
+end
+
+menu[:specials].each do |menu_item|
+  file = menu_item.delete(:image)
+  f = MenuItem.create(menu_item)
+  f.price_cents = menu_item[:price] * 100
+  f.image.attach(io: File.open(file), filename: "#{f.id}.jpeg", content_type: 'image/jpeg')
+  puts "Created menu item #{menu_item[:name]}"
+end
+
+menu[:koek_en_taart].each do |menu_item|
   file = menu_item.delete(:image)
   f = MenuItem.create(menu_item)
   f.price_cents = menu_item[:price] * 100
@@ -65,7 +88,7 @@ menu[:koude_dranken].each do |menu_item|
   puts "Created menu item #{menu_item[:name]}"
 end
 
-menu[:desserts].each do |menu_item|
+menu[:warme_dranken].each do |menu_item|
   file = menu_item.delete(:image)
   f = MenuItem.create(menu_item)
   f.price_cents = menu_item[:price] * 100
